8e0f28ac692ff5acfd33fb18e06a4af9114a77c3
VelizarDabov <velizardabov@gmail.com>
2024-01-05 01:07:47 +0200

    sidebar ready

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Gmail clone</h1>
+      <Sidebar />
     </div>
   );
 }
